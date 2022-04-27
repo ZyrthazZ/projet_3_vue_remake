f254b08e054e7e0ea1aec85e25571aaa9b5e58a3
ZyrthaZ <jeanseb.chevalier@gmail.com>
2022-04-27 12:40:27 +0200

    Commit fichiers depuis laptop

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <img src="../src/assets/img/logo/ohmyfood.png" alt="logo oh my food" class="header__logo">
+    <div class="header__loc">
+      <span class="header__loc-map">
+        <i class="fas fa-map-marker-alt"></i>
+      </span>
+      <p class="header__loc-text">Paris, Belleville</p>
+    </div>
+  </header>
+  <footer>
+    <img src="../src/assets/img/logo/ohmyfood.png" alt="logo oh my food" class="footer_logo">
+  </footer>
+
+  <RouterView />
+</template>
+
+<style lang="scss">
+header {
+  text-align: center;
+}
+
+.header__logo {
+  padding-top: 15px;
+  padding-bottom: 15px;
+}
+</style>
